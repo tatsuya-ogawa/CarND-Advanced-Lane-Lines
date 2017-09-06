f733de5f07cf5e0e851a67d1da2d2b05f1675727
Tatsuya_Ogawa <Tatsuya_Ogawa@jmas.co.jp>
2017-09-06 21:42:41 +0900

    first commit.

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def dummy():
         cv2.destroyAllWindows()
 
 
-images = glob.glob('./test_images/straight_lines*.jpg')
+images = glob.glob('./test_images/*.jpg')
 
 
 # Define a function that takes an image, gradient orientation,
@@ -82,33 +82,132 @@ def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
     return combined_binary
 
 
-for fname in images:
-    img = cv2.imread(fname)
-    grad_binary = pipeline(img)
-    # Plot the result
-    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-    f.tight_layout()
+def getWarpedImage(img):
     shape = img.shape
     src = np.float32([
-        [1170 - 450, shape[0] - 280],
+        [1170 - 480, shape[0] - 280],
         [1170, shape[0]],
         [150, shape[0]],
-        [150 + 410, shape[0] - 280],
+        [150 + 450, shape[0] - 280],
     ])
     dst = np.float32([
-        [1170, shape[0] - 280],
-        [1170, shape[0]],
-        [150, shape[0]],
-        [150, shape[0] - 280],
+        [1170 - 480, shape[0] - 280],
+        [1170 - 480, shape[0]],
+        [150 + 450, shape[0]],
+        [150 + 450, shape[0] - 280],
     ])
     M = cv2.getPerspectiveTransform(src, dst)
     Minv = cv2.getPerspectiveTransform(dst, src)
-    warped = cv2.warpPerspective(img, M, img.shape[:2][::-1], flags=cv2.INTER_LINEAR)
+    grad_binary = pipeline(img)
+    # grad_binary = np.dstack((grad_binary, grad_binary, grad_binary))
+    warped = cv2.warpPerspective(grad_binary, M, img.shape[:2][::-1], flags=cv2.INTER_LINEAR)
+    return warped[-280:,575:725]
+    #return warped[-280:,600:690]
+    return warped
+
+
+def sliding_window(binary_warped):
+    # Assuming you have created a warped binary image called "binary_warped"
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[int(binary_warped.shape[0] / 2):, :], axis=0)
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0] / 2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0] / nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 10
+    # Set minimum number of pixels found to recenter window
+    minpix = 25
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window + 1) * window_height
+        win_y_high = binary_warped.shape[0] - window * window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high),
+                      (0, 255, 0), 2)
+        cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high),
+                      (0, 255, 0), 2)
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                          (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                           (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0] - 1, binary_warped.shape[0])
+    left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+    right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    plt.imshow(out_img)
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+    plt.show()
+
+for fname in images:
+    img = cv2.imread(fname)
+
+    # Plot the result
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+    f.tight_layout()
+
     ax1.imshow(img)
     ax1.set_title('Original Image', fontsize=50)
-    ax2.imshow(warped)
+    warped = getWarpedImage(img)
+    ax2.imshow(warped, cmap='gray')
     ax2.set_title('Warped Image')
-    # ax2.imshow(grad_binary, cmap='gray')
-    # ax2.set_title('Thresholded Gradient', fontsize=50)
+    #ax2.imshow(grad_binary, cmap='gray')
+    #ax2.set_title('Thresholded Gradient', fontsize=50)
+
     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
     plt.show()
+    sliding_window(warped)
